3bfc5cfcc63604141c5651f8806ab548a8fb90f2
Helbertt Dalarme <hdalarme@yahoo.com.br>
2023-10-09 11:08:27 -0300

    Update desafio.kt

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,40 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
-
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
+class Usuario(val nome: String)
 
 data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
 
 data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
 
     val inscritos = mutableListOf<Usuario>()
-    
+
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
+        println("Usuário ${usuario.nome} matriculado na formação $nome.")
+    }
+
+    fun exibirInscritos() {
+        println("Inscritos na formação $nome:")
+        inscritos.forEach { println(it.nome) }
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    // Criação de usuários
+    val usuario1 = Usuario("João")
+    val usuario2 = Usuario("Maria")
+
+    // Criação de conteúdos educacionais
+    val conteudo1 = ConteudoEducacional("Introdução ao Kotlin", 120)
+    val conteudo2 = ConteudoEducacional("Programação Orientada a Objetos em Kotlin", 180)
+
+    // Criação de uma formação
+    val formacaoKotlin = Formacao("Formação Kotlin", listOf(conteudo1, conteudo2))
+
+    // Matrícula de usuários na formação
+    formacaoKotlin.matricular(usuario1)
+    formacaoKotlin.matricular(usuario2)
+
+    // Exibição de inscritos na formação
+    formacaoKotlin.exibirInscritos()
 }
